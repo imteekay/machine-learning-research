73662bd2d59ce6b88d55de9c53cff4cce3c84df0
Leandro Tk <leandrotk100@gmail.com>
2017-12-31 12:19:09 -0200

    Intro to numpy 1

---
+++ numpy/intro1.py
@@ -0,0 +1,10 @@
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# import numpy as np
+import numpy as np
+
+# Create a numpy array based on baseball list
+np_baseball = np.array(baseball)
+
+# print the basecall list type
+print(type(np_baseball))
