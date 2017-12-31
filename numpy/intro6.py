eef60c5ab1816b605cfd86077ea71e2486b73947
Leandro Tk <leandrotk100@gmail.com>
2017-12-31 12:19:36 -0200

    Intro to numpy 6

---
+++ numpy/intro6.py
@@ -0,0 +1,15 @@
+# height and weight are available as a regular lists
+height = [43, 53, 65, 54, 62, 99]
+
+# Import numpy
+import numpy as np
+
+# Store weight and height lists as numpy arrays
+np_weight = np.array(weight)
+np_height = np.array(height)
+
+# Print out the weight at index 50
+print(np_weight[50])
+
+# Print out sub-array of np_height: index 100 up to and including index 110
+print(np_height[100:111])
