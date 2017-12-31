252f52eab2baca3469a8b2ea34307dcd64943d86
Leandro Tk <leandrotk100@gmail.com>
2017-12-31 16:32:37 -0200

    Intro to numpy 9

---
+++ numpy/intro9.py
@@ -0,0 +1,16 @@
+# baseball is available as a regular list of lists
+
+# Import numpy package
+import numpy as np
+
+# Create np_baseball (2 cols)
+np_baseball = np.array(baseball)
+
+# Print out the 50th row of np_baseball
+print(np_baseball[49,:])
+
+# Select the entire second column of np_baseball: np_weight
+np_weight = np_baseball[:,1]
+
+# Print out height of 124th player
+print(np_baseball[123,0])
