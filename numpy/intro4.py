6a818480c8d5a180019238cc2fb79f51d08583c7
Leandro Tk <leandrotk100@gmail.com>
2017-12-31 12:19:25 -0200

    Intro to numpy 4

---
+++ numpy/intro4.py
@@ -0,0 +1,19 @@
+# height and weight are available as a regular lists
+height = [43, 53, 65, 54, 62, 99]
+
+# Import numpy
+import numpy as np
+
+# Calculate the BMI: bmi
+np_height_m = np.array(height) * 0.0254
+np_weight_kg = np.array(weight) * 0.453592
+bmi = np_weight_kg / np_height_m ** 2
+
+# Create the light array
+light = bmi < 21
+
+# Print out light
+print(light)
+
+# Print out BMIs of all baseball players whose BMI is below 21 | Subsetting
+print(bmi[light])
