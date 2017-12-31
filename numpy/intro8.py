cd4bdc6558ec59d246052a3742b3c22705e1a4db
Leandro Tk <leandrotk100@gmail.com>
2017-12-31 16:21:16 -0200

    Intro to numpy 8

---
+++ numpy/intro8.py
@@ -0,0 +1,10 @@
+# baseball is available as a regular list of lists
+
+# Import numpy package
+import numpy as np
+
+# Create a 2D numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out the shape of np_baseball
+print(np_baseball.shape)
