304f4672d13c5ff3624385735d3bd9ed8439e067
Leandro Tk <leandrotk100@gmail.com>
2017-12-31 16:33:21 -0200

    Adding matrix baseball variable

--- numpy/intro9.py
+++ numpy/intro9.py
@@ -1,4 +1,8 @@
 # baseball is available as a regular list of lists
+baseball = [[180, 78.4],
+            [215, 102.7],
+            [210, 98.5],
+            [188, 75.2]]
 
 # Import numpy package
 import numpy as np
