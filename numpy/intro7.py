cbf50ca494945968ec550594fabbce058ba7cbf3
Leandro Tk <leandrotk100@gmail.com>
2017-12-31 16:16:57 -0200

    Intro to numpy 7

---
+++ numpy/intro7.py
@@ -0,0 +1,17 @@
+# Create baseball, a list of lists
+baseball = [[180, 78.4],
+            [215, 102.7],
+            [210, 98.5],
+            [188, 75.2]]
+
+# Import numpy
+import numpy as np
+
+# Create a 2D numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out the type of np_baseball
+print(type(np_baseball))
+
+# Print out the shape of np_baseball
+print(np_baseball.shape)
