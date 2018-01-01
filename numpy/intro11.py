25f61871ca21edf17952e0d60bb81837eb57d15f
Leandro Tk <leandrotk100@gmail.com>
2018-01-01 12:57:24 -0200

    Intro to numpy 11

---
+++ numpy/intro11.py
@@ -0,0 +1,13 @@
+# np_baseball is available
+
+# Import numpy
+import numpy as np
+
+# Create np_height from np_baseball
+np_height = np_baseball[:,0]
+
+# Print out the mean of np_height
+print(np.mean(np_height))
+
+# Print out the median of np_height
+print(np.median(np_height))
