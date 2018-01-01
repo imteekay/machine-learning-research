d28d322c46a6c319425adb4a35d53adb9ae40241
Leandro Tk <leandrotk100@gmail.com>
2017-12-31 22:22:19 -0200

    Intro to numpy 10

---
+++ numpy/intro10.py
@@ -0,0 +1,17 @@
+# baseball is available as a regular list of lists
+# updated is available as 2D numpy array
+
+# Import numpy package
+import numpy as np
+
+# Create np_baseball (3 cols)
+np_baseball = np.array(baseball)
+
+# Print out addition of np_baseball and updated
+print(np_baseball + updated)
+
+# Create numpy array: conversion
+conversion = np.array([0.0254, 0.453592, 1])
+
+# Print out product of np_baseball and conversion
+print(np_baseball * conversion)
