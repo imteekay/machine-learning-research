2415026816c981b295482bfc5721d8e4e7416a85
TK <leandrotk100@gmail.com>
2024-06-29 14:46:00 -0300

    *

--- rosalind/fibd.py
+++ rosalind/fibd.py
@@ -8,7 +8,7 @@ def fibd(n, m):
     new_born = sum(ages[1:]) # The 0th generation can't reproduce
 
     # after the m-th-month, the rabbits die
-    # shifting value will stop counting the died rabbits
+    # shifting value will stop counting the dead rabbits
     for i in range(m - 1, 0, -1):
       ages[i] = ages[i - 1]
     
